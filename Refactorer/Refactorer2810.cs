975c99fb2975f1b982d4e5cfd5d24c87643c20b2
VladKrp <vladkrp2003@gmail.com>
2023-09-12 13:30:58 +0300

    Method interface added

---
+++ Refactorer/Refactorer2810.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactorer
+{
+    public static class Refactorer2810
+    {
+        public static string ExtractConstant(string text)
+        {
+            return string.Empty;
+        }
+
+        public static string RenameMethod(string oldName, string newName, string text)
+        {
+            return string.Empty;
+        }
+
+        public static string RemoveUnusedParameters(string text)
+        {
+            return string.Empty;
+        }
+    }
+}

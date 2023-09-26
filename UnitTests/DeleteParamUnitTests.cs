226cc5a43ba175014559e1bdeeb921265fa204ae
Mr.PainKiller <109076089+MrPainKiller@users.noreply.github.com>
2023-09-26 08:23:09 +0300

    DeleteUnused params - add comments

--- UnitTests/DeleteParamUnitTests.cs
+++ UnitTests/DeleteParamUnitTests.cs
@@ -7,7 +7,7 @@ namespace UnitTests
     [TestClass]
     public class DeleteParamUnitTests
     {
-        // 1
+        // 1 - Видалення невикористовуваного параметра - звичайний випадок
         [TestMethod]
         public void DeleteParam_BaseCase_Successfully()
         {
@@ -42,7 +42,8 @@ namespace UnitTests
         }
         */
 
-        // 2
+        // 2 - Видалення невикористовуваного параметра,
+        //     якщо ім'я параметра збігається з назвою функції
         [TestMethod]
         public void DeleteParam_FunctionNameEqualParamName_DeleteParamName()
         {
@@ -52,7 +53,8 @@ namespace UnitTests
             Assert.AreEqual(expectedOutput,res);
         }
 
-        // 3
+        // 3 - Видалення невикористовуваного параметра
+        //     зі значення за замовчуванням (тобто не використовується)
         [TestMethod]
         public void UnusedParam_with_default_value()
         {
@@ -62,7 +64,8 @@ namespace UnitTests
             Assert.AreEqual(expectedOutput, res);
         }
 
-        // 4
+        // 4 - Видалення невикористовуваного параметра,
+        //     якщо назва параметра фігурує тільки у рядковій константі
         [TestMethod]
         public void UnusedParam_used_in_string_constant()
         {
@@ -72,7 +75,8 @@ namespace UnitTests
             Assert.AreEqual(expectedOutput, res);
         }
 
-        // 5
+        // 5 - Видалення невикористовуваного параметра,
+        //     якщо ім'я параметра збігається з назвою методу класу
         [TestMethod]
         public void UnusedParamName_same_as_classMethod_name()
         {
@@ -82,19 +86,19 @@ namespace UnitTests
             Assert.AreEqual(expectedOutput, res);
         }
 
-        // 6
+        // 6 - параметр вважається використовуваним,
+        //     якщо параметр - об'єкт класу і є виклик методу/процедури
         [TestMethod]
         public void ClassObjectParam_used_in_funcion()
         {
-            // якщо викликається метод класу (який передали через параметер),
-            // то він вважається used.
             string input = "void func(MyClass obj)\r\n{\r\n\tobj.Func();\r\n}";
             string expectedOutput = "void func(MyClass obj)\r\n{\r\n\tobj.Func();\r\n}";
             var res = Refactorer2810.RemoveUnusedParameters(input);
             Assert.AreEqual(expectedOutput, res);
         }
 
-        // 7
+        // 7 - Видалення невикористовуваного параметра,
+        //     якщо існує параметр з такою самою назвою але іншим letter case 
         [TestMethod]
         public void TwoParams_in_differet_cases()
         {
@@ -104,7 +108,7 @@ namespace UnitTests
             Assert.AreEqual(expectedOutput, res);
         }
 
-        // 8
+        // 8 - Видалення невикористовуваного параметра, якщо його назва збігається з назвою класу
         [TestMethod]
         public void UnusedParam_has_same_name_as_className()
         {
